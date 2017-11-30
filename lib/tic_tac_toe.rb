4b02ecd469ca74eefa73c4d8db413fef2376378e
Linda ONeill <oneill.linda21@gmail.com>
2017-11-30 19:17:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ def turn
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(input)
-      move(input, current_player)
+      move(index, current_player)
       display_board
     else
       turn
