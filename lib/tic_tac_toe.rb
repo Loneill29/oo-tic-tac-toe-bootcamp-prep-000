826b59a8016a0e108864d4fa27e978355a0c96ee
Linda ONeill <oneill.linda21@gmail.com>
2017-11-30 18:42:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,13 +32,11 @@ class TicTacToe
     end
   end
 
-def valid_move?(position)
-    position = position.to_i - 1
-    if position.between?(0,8) && !position_taken?(position)
-      true
-    else
-      false
-    end
+  def valid_move?(location)
+    if location.between?(0,8) && !position_taken?(location)
+     true
+  end
+  else false
   end
 
 end
