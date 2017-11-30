ab4855c524f599b899ae88d2faed50749234af9b
Linda ONeill <oneill.linda21@gmail.com>
2017-11-30 18:29:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ class TicTacToe
       def input_to_index(user_input)
         user_input.to_i - 1
       end
-      def move(index, current_player + "X")
+      def move(index, current_player = "X")
       @board[index] = current_player
     end
 
