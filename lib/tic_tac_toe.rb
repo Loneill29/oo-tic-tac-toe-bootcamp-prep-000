ffbbbf9e087bb80eee320067cc4204e013d4ba0f
Linda ONeill <oneill.linda21@gmail.com>
2017-11-30 18:48:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,4 +50,13 @@ def turn
     display_board
   end
 
+  def turn_count(board)
+    count = 0
+    board.each do |move|
+      if move == "X" || move == "O"
+        count += 1
+    end
+  end
+    count
+  end
 end
