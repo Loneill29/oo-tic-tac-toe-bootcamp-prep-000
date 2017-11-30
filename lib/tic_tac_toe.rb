de45dec011c52fe3526f35e64acc60a30dd4050c
Linda ONeill <oneill.linda21@gmail.com>
2017-11-30 18:27:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,9 @@ class TicTacToe
         puts "-----------"
         puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
       end
-
+      def input_to_index
+        user_input.to_i - 1
+      end
 
 
 
